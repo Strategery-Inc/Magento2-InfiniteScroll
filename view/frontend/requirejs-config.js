2c65df610e255b7fbf324f84e3b86230e4eca5df
Damian A. Pastorini <damian.pastorini@usestrategery.com>
2016-07-18 12:54:44 -0300

    - InfiniteScroll - Magento 2

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            infinitescroll: 'Strategery_Infinitescroll/js/infinitescroll',
+            jqueryIas: 'Strategery_Infinitescroll/js/jquery-ias.min'
+        }
+    }
+};
---
+++ view/frontend/web/js/infinitescroll.js
@@ -0,0 +1,7 @@
+define([
+    "jqueryIas",
+    "infinitescroll"
+], function($, jqueryIas, infinitescroll) {
+    "use strict";
+    // console.log('InfiniteScroll is here!');
+});
