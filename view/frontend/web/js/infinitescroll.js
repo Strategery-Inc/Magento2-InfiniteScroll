Strategery-Inc/Magento2-InfiniteScroll

commit 27fe1463c08e7d9d58fe59c1cb7e785383e2b2e4

Fix for Ajax add to Cart on Ajax loaded products
x magento init scripts are not initialised when loaded over ajax. This fixes the ajax add to cart buttons once the new products are rendered

view/frontend/web/js/infinitescroll.js
@@ -87,6 +87,7 @@ define([
                 });
                 window.ias.on('rendered', function(items){
                     SgyIAS._log({eventName: 'render', items: items});
+                    $("form[data-role='tocart-form']").catalogAddToCart();
                 });
                 window.ias.on('noneLeft', function(){
                     SgyIAS._log({eventName: 'noneLeft'});

commit b8077f0b3c8d451ec5e951267d91ec8999ae6d7e

Fixed possible JS error caused by commit 27fe1463c08e7d9d58fe59c1cb7e785383e2b2e4.

view/frontend/web/js/infinitescroll.js
@@ -87,7 +87,9 @@ define([
                 });
                 window.ias.on('rendered', function(items){
                     SgyIAS._log({eventName: 'render', items: items});
-                    $("form[data-role='tocart-form']").catalogAddToCart();
+                    if ( $("form[data-role='tocart-form']").length ) {
+                        $("form[data-role='tocart-form']").catalogAddToCart();
+                    }
                 });
                 window.ias.on('noneLeft', function(){
                     SgyIAS._log({eventName: 'noneLeft'});